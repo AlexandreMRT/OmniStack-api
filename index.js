7a29ad59d495cd2a2f492a0865589fa51a43019e
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-01-14 23:45:27 -0300

    Start of the project

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+// metodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parametros
+
+// Query Params: request.query (Filtros, ordenacao, paginacao)
+// Route Params: request.params (Identificar um recurso na alteracao ou remocao)
+// Body: request.body (Dados para ciracao ou alteracao de um registro)
+
+app.post('/users/:id', (request, response) => {
+  console.log(request.body);
   return response.json({ message: 'Hello OmniStack' });
 });
 
