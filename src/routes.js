e813c934bdae7affd48c77a8b453218007e6ace7
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-01-15 00:48:51 -0300

    Request data from github api

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ const axios = require('axios');
 
 const routes = Router();
 
-routes.post('/devs', (request, response) => {
+routes.post('/devs', async (request, response) => {
   const { github_username } = request.body;
 
+  const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+  console.log(apiResponse.data);
 
   return response.json({ message: 'Hello OmniStack' });
 });
