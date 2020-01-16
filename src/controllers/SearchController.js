545d82f829d150173bf9b2cdfb0bd1420137b79c
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-01-15 22:30:56 -0300

    Add SearchController controller

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,16 @@
+const Dev = require ('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+  async index(request, response){
+    // Buscar todos os devs num raio de 10km
+    // Filtrar por tecnologia
+    const { latitude, longitude, techs  } = request.query;
+
+    const techsArray = parseStringAsArray(techs);
+
+    console.log(techsArray);
+
+    return response.json({ devs: [] });
+  }
+}
