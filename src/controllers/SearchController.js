902b772a6802a68dfb064ca5825bec03a77c43b1
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-01-15 22:48:50 -0300

    Add const to filter devs based on techs and location to SearchController

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -9,8 +9,20 @@ module.exports = {
 
     const techsArray = parseStringAsArray(techs);
 
-    console.log(techsArray);
-
-    return response.json({ devs: [] });
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    });
+    return response.json({ devs });
   }
 }
